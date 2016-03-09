b7a803d24bde3c5eab1881269da929ea1bf9e239
Nec Met Tin <calisankazanir@gmail.com>
2016-03-09 12:33:55 +0200

    Tekrar yükle

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace kisaYol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtimza = new System.Windows.Forms.RichTextBox();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnYenile = new System.Windows.Forms.Button();
+            this.btnGizle = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtimza
+            // 
+            this.txtimza.Location = new System.Drawing.Point(12, 63);
+            this.txtimza.Name = "txtimza";
+            this.txtimza.Size = new System.Drawing.Size(611, 313);
+            this.txtimza.TabIndex = 0;
+            this.txtimza.Text = "";
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(362, 382);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(81, 23);
+            this.btnGuncelle.TabIndex = 1;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnYenile
+            // 
+            this.btnYenile.Location = new System.Drawing.Point(275, 382);
+            this.btnYenile.Name = "btnYenile";
+            this.btnYenile.Size = new System.Drawing.Size(81, 23);
+            this.btnYenile.TabIndex = 2;
+            this.btnYenile.Text = "Yenile";
+            this.btnYenile.UseVisualStyleBackColor = true;
+            this.btnYenile.Click += new System.EventHandler(this.btnYenile_Click);
+            // 
+            // btnGizle
+            // 
+            this.btnGizle.Location = new System.Drawing.Point(12, 382);
+            this.btnGizle.Name = "btnGizle";
+            this.btnGizle.Size = new System.Drawing.Size(207, 23);
+            this.btnGizle.TabIndex = 3;
+            this.btnGizle.Text = "Programı Gizle";
+            this.btnGizle.UseVisualStyleBackColor = true;
+            this.btnGizle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(482, 387);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Copyright © babayarisi 2016";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 8);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(610, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 409);
+            this.ControlBox = false;
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGizle);
+            this.Controls.Add(this.btnYenile);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.txtimza);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.7D;
+            this.Text = "notepad#";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtimza;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnYenile;
+        private System.Windows.Forms.Button btnGizle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kisaYol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            this.ShowIcon = false;
+            Boolean kopyala = Form1.RegisterHotKey(this.Handle, 0, 0x0001, 0x43);
+            Boolean goster = Form1.RegisterHotKey(this.Handle, 1, 0x0001, 0x41);
+            string imzaici = System.IO.File.ReadAllText("imza.txt");
+            txtimza.Text = imzaici;
+        }
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        //[DllImport("user32.dll")]
+        //public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312)
+            {
+                if (m.WParam.ToInt32() == 0) 
+                {
+                    Clipboard.SetText(txtimza.Text);
+                }
+                if (m.WParam.ToInt32() == 1)
+                {
+                    if (this.WindowState == FormWindowState.Minimized)
+                    {
+                        this.WindowState = FormWindowState.Normal;
+                    }
+                    else
+                    {
+                        this.WindowState = FormWindowState.Minimized;
+                    }
+                    
+                }
+                
+
+                 
+            }
+            base.WndProc(ref m);
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Maximum = 100;
+            trackBar1.Minimum = 5;
+            trackBar1.Value = 70;
+        }
+
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter("imza.txt");
+            file.WriteLine(txtimza.Text);
+            MessageBox.Show("Güncellendi");
+            file.Close();
+        }
+
+        private void btnYenile_Click(object sender, EventArgs e)
+        {
+            string imzaici = System.IO.File.ReadAllText("imza.txt");
+            txtimza.Text = imzaici;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Opacity = (double)trackBar1.Value / 100;
+        }
+    }
+}
